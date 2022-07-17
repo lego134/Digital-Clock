210f1720dfbb03c0b6d866dccd24c7c78584d0a7
Jonah Raiford <xiafei44@icloud.com>
2022-07-16 19:36:02 -0600

    My first problem that took 2 hour to solve!

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ function UpdateClock(){
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     let y = new Date().getUTCFullYear();
-    let d = new Date().getDay();
-    let getMonth = new Date().getMonth();
     let ampm = "AM";
-
-
+    const month_a = [1,2,3,4,5,6,7,8,9,10,11,12];
+    const Month_Computer = new Date();
+    let getMonth = month_a[Month_Computer.getMonth()];
+    MonthEl.innerHTML = getMonth;
+    let day_a = new Date();
+    
+DayEl.innerHTML = day_a.getDate();
+    
     if (h > 12){
         h = h- 12;
         ampm = "PM";
@@ -22,15 +26,14 @@ function UpdateClock(){
     h = h < 10 ? "0" + h: h;
     m = m < 10 ? "0" + m: m;
     s = s < 10 ? "0" + s: s;
-    d = d < 10 ? "0" + d: d;
-    getMonth = getMonth < 10 ? "0" + getMonth: getMonth;
     Hours.innerText = h;
     Minutes.innerText = m;
     Secound.innerText = s;
     amPmEl.innerText = ampm;
     YearsEl.innerText = y;
-    DayEl.innerText = d;
-    MonthEl.innerText = getMonth;
+ 
+   
+
 
     setInterval(function(){ 
         UpdateClock()
